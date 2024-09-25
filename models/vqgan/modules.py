f8f60cbdcaae143c8fd1bf9a283a146056283afe
yjh0410 <1394571815@qq.com>
2024-09-25 22:40:47 +0800

    use r50 as the LPIPS & add NLB

--- models/vqgan/modules.py
+++ models/vqgan/modules.py
@@ -45,6 +45,42 @@ class DeConvModule(nn.Module):
         return x
 
 
+# ----------- Attention modules -----------
+class NonLocalBlock(nn.Module):
+    def __init__(self, in_dim : int):
+        super(NonLocalBlock, self).__init__()
+        self.in_dim = in_dim
+
+        self.qkv_proj = nn.Linear(in_dim, in_dim * 3, bias=False)
+        self.proj_out = nn.Linear(in_dim, in_dim, bias=False)
+        self.norm = nn.LayerNorm(in_dim)
+
+    def forward(self, x):
+        shortcut = x
+        bs, c, h, w = x.shape
+
+        # [bs, c, h, w] -> [bs, c, hw] -> [bs, hw, c]
+        x = x.view(bs, c, -1).permute(0, 2, 1).contiguous()
+        x = self.norm(x)
+
+        # QKV projection
+        qkv = self.qkv_proj(x)
+        q, k, v = torch.chunk(qkv, chunks=3, dim=-1)
+
+        scores = q @ k.transpose(1, 2).contiguous()  # [bs, hw, hw]
+        scores = scores * (c**(-0.5))
+        scores = F.softmax(scores, dim=-1)
+
+        out = scores @ v  # [bs, hw, c]
+        out = self.proj_out(out)
+
+        # [bs, hw, c] -> [bs, c, hw] -> [bs, c, h, w]
+        out = out.permute(0, 2, 1).contiguous().view(bs, c, h, w)
+        out = out + shortcut
+
+        return out
+    
+    
 # ----------- ResNet modules -----------
 class ResBlock(nn.Module):
     def __init__(self, in_dim, out_dim, shortcut=True):
